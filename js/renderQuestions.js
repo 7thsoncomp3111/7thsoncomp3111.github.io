81989d5496a6b31901a388842e69d28fd969340d
johnhorsema <jjmace01@gmail.com>
2015-10-05 01:36:57 +0800

    First Commit
    Replicated design from Ray's designs.

---
+++ js/renderQuestions.js
@@ -0,0 +1,19 @@
+//Pre-compile
+var source   = $("#questionTemplate").html();
+var feed = $("#questionTemplateTop").html();
+var template = Handlebars.compile(source);
+var feedTemplate = Handlebars.compile(feed);
+//Execution
+var context = [
+              {title: "My First Post", body: "Who is the founder of Google?"},
+              {title: "Another Post", body: "Who is the founder of Microsoft?"},
+              {title: "Picture Post", body: "<img src='images/seagull-205528_640.jpg'>"}
+              ];
+var html;
+for(var i=0; i<context.length; i++){
+  html = template(context[i]);
+  $("#questionList").append(html);
+}
+// Pseudo: sort posts with an algorithm
+html = feedTemplate(context[context.length-1]);
+$("#questionListTop").append(html);
