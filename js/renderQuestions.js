99e8ed835cf8f5187eaab8d37027a168299eef81
johnhorsema <jjmace01@gmail.com>
2015-10-06 02:15:39 +0800

    Added date and comments
    Also added highlight of a question 'posted just now'
    Unable to simulate a fade out animation though

--- js/renderQuestions.js
+++ js/renderQuestions.js
@@ -5,17 +5,24 @@ var template = Handlebars.compile(source);
 var feedTemplate = Handlebars.compile(feed);
 //Execution
 var context = [
-              {title: "My First Post", body: "Who is the founder of Google?"},
-              {title: "Another Post", body: "Who is the founder of Microsoft?"},
-              {title: "Picture Post", body: "<img src='images/seagull-205528_640.jpg'>"}
+              {title: "Picture Post", body: "<img src='images/seagull-205528_640.jpg'>", time: "posted just now"},
+              {title: "Another Post", body: "Who is the founder of Microsoft?", time: "posted 3 days ago"},
+              {title: "My First Post", body: "Who is the founder of Google?", time: "posted 1 week ago"}
               ];
 var html;
 for(var i=0; i<context.length; i++){
   context[i].index = i;
   html = template(context[i]);
+  if(context[i].time.indexOf("now")!=-1){
+    html = html.replace("question","question now");
+  }
   $("#questionList").append(html);
 
 }
 // Pseudo: sort posts with an algorithm
-html = feedTemplate(context[context.length-1]);
+html = feedTemplate(context[0]);
 $("#questionListTop").append(html);
+
+//Format comments
+voting = '<div class="voting"><a href="" class="glyphicon glyphicon-thumbs-up up"></a> <a href="" class="glyphicon glyphicon-thumbs-down down"></a></div>';
+$(".comment").append(voting);
